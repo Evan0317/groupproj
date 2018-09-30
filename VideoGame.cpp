a1ad17eb9f623f087252912d242c47b84f57a589
Evan0317 <32783248+Evan0317@users.noreply.github.com>
2018-09-29 21:04:55 -0400

    Update VideoGame.cpp

--- VideoGame.cpp
+++ VideoGame.cpp
@@ -1,26 +1,39 @@
-#include "VideoGame.h"
-#include <iostream>
+#pragma once
+#include <string>
 using namespace std;
 
-//Default Constructor Function Definition:
-VideoGame::VideoGame()
+class VideoGame
 {
-	title = "";
-	console = "";
-	price = 0;
-	releaseYear = 0;
-	numberInStock = 0;
-}
+private:
+	string title;
+	string console;
+	double price;
+	int releaseYear;
+	int numberInStock;
+public:
+	//CONSTRUCTORS:
+	VideoGame();		//Default Constructor
+	VideoGame(string t, string c, double p, int year, int number);		//- uncomment after defined
 
-//*Function for second constructor (with parameters) goes here*
-//**Functions to increase/decrease "number in stock" go here**
+	//SET AND GET FUNCTIONS:		
+	void setTitle(string t) { title = t; }
+	void setConsole(string c) { console = c; }
+	void setPrice(double p) { price = p; }
+	void setReleaseYear(int y) { releaseYear = y; }
+	void setNumberInStock(int n) { numberInStock = n;  }
 
-//Display Function Definition:
-void VideoGame::display()
-{
-	cout << "Title: " << getTitle() << endl;
-	cout << "Console: " << getConsole() << endl;
-	cout << "Price: $" << getPrice() << endl;
-	//cout << "Release Year: " << getReleaseYear() << endl;					- needs set/get functions
-	//cout << "Number In Stock: " << getNumberInStock() << endl << endl;	- needs set/get functions
-}
+	string getTitle() { return title; }
+	string getConsole() { return console; }
+	double getPrice() { return price; }
+	int getReleaseYear() { return releaseYear; }
+	int getNumberInStock() { return numberInStock; }
+
+
+	////INCREASE + DECREASE FUNCTIONS:
+	void increaseStock(int number); 	// increase numberInStock by number
+	void decreaseStock(int number) { number = 10, numberInStock = numberInStock - number; }// decrease numberInStock by number
+
+	//DISPLAY FUNCTION:
+	void display();
+};
+
