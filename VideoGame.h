6b9d1513d9ef25095c5c275107a3ac28811585e7
Evan0317 <32783248+Evan0317@users.noreply.github.com>
2018-09-29 21:05:35 -0400

    Update VideoGame.h

--- VideoGame.h
+++ VideoGame.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <string>
 using namespace std;
 
@@ -9,32 +10,30 @@ private:
 	double price;
 	int releaseYear;
 	int numberInStock;
-	
 public:
 	//CONSTRUCTORS:
-		VideoGame();														//Default Constructor
-	//	VideoGame(string t, string c, double p, int year, int number);		- uncomment after defined
-	
-	//SET AND GET FUNCTIONS:		
-		void setTitle(string t)				{ title = t; }
-		void setConsole(string c)			{ console = c; }
-		void setPrice(double p)				{ price = p; }
-		void setReleaseYear(int year);
-		void setNumberInStock(int number);
+	VideoGame();		//Default Constructor
+	VideoGame(string t, string c, double p, int year, int number);		//- uncomment after defined
 
-		string getTitle()		{ return title; }
-		string getConsole()		{ return console; }
-		double getPrice()		{ return price; }
-		int getReleaseYear();
-		int getNumberInStock();
-	
+	//SET AND GET FUNCTIONS:		
+	void setTitle(string t) { title = t; }
+	void setConsole(string c) { console = c; }
+	void setPrice(double p) { price = p; }
+	void setReleaseYear(int y) { releaseYear = y; }
+	void setNumberInStock(int n) { numberInStock = n;  }
+
+	string getTitle() { return title; }
+	string getConsole() { return console; }
+	double getPrice() { return price; }
+	int getReleaseYear() { return releaseYear; }
+	int getNumberInStock() { return numberInStock; }
+
 
 	////INCREASE + DECREASE FUNCTIONS:
-	//	void increaseStock(int number);	// increase numberInStock by number
-	//	void decreaseStock(int number);	// decrease numberInStock by number
-	//
+	void increaseStock(int number); 	// increase numberInStock by number
+	void decreaseStock(int number) { number = 10, numberInStock = numberInStock - number; }// decrease numberInStock by number
 
 	//DISPLAY FUNCTION:
-		void display();
-
+	void display();
 };
+
